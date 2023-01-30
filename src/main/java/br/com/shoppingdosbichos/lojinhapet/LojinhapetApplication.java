a0797a5a503607a5637915686d60804d4a36da4d
paulo <pbrsj1385@gmail.com>
2023-01-30 11:08:37 -0300

    inicia projeto

---
+++ src/main/java/br/com/shoppingdosbichos/lojinhapet/LojinhapetApplication.java
@@ -0,0 +1,13 @@
+package br.com.shoppingdosbichos.lojinhapet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojinhapetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojinhapetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/shoppingdosbichos/lojinhapet/LojinhapetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.shoppingdosbichos.lojinhapet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojinhapetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
