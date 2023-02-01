08c15a267fcde5d9235dbb7d72ef06f341e5212c
paulo <pbrsj1385@gmail.com>
2023-02-01 10:48:08 -0300

    implementa dominio de negocio

--- src/main/java/br/com/shoppingdosbichos/lojinhapet/entity/Cliente.java
+++ src/main/java/br/com/shoppingdosbichos/lojinhapet/entity/Cliente.java
@@ -1,16 +1,19 @@
 package br.com.shoppingdosbichos.lojinhapet.entity;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Cliente {
 	
 	private String nomeCompleto;
 	private String telefone;
 	private String cpf;
 	
-	public Cliente(String nomeCompleto, String telefone, String cpf) {
-		super();
+	private Cliente(String nomeCompleto, String telefone, String cpf) {
+		
 		this.nomeCompleto = nomeCompleto;
 		this.telefone = telefone;
 		this.cpf = cpf;
